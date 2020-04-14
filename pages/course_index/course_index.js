d699ac2ef0bbf857a9e794e6bed9bf6cdca92e0a
hmlr123 <hmlr123@163.com>
2020-04-14 21:26:16 +0800

    初步工程demo

---
+++ pages/course_index/course_index.js
@@ -0,0 +1,87 @@
+// pages/course_index/course_index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    selected: 0,
+    list: ['全部课程', '我听的课', '我教的课'],
+  },
+  //tab框
+  selected: function (e) {
+    let that = this
+    console.log(e)
+    let index = e.currentTarget.dataset.index
+    console.log("index",index)
+    if (index == 0) {
+      that.setData({
+        selected: 0
+      })
+    } else if (index == 1) {
+      that.setData({
+        selected: 1
+      })
+    } else {
+      that.setData({
+        selected: 2
+      })
+    } 
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
