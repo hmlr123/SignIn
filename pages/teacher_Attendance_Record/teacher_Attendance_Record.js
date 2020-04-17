53cebc6bae6b38e69ca8de046be4d068ae226cd5
hmlr123 <hmlr123@163.com>
2020-04-18 02:54:08 +0800

    李欢欢的代码

--- pages/teacher_Attendance_Record/teacher_Attendance_Record.js
+++ pages/teacher_Attendance_Record/teacher_Attendance_Record.js
@@ -1,32 +1,11 @@
-// pages/course_index/course_index.js
+// pages/teacher_Attendance_Record/teacher_Attendance_Record.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    selected: 0,
-    list: ['我教的课','我听的课'],
-  },
-  //tab框
-  selected: function (e) {
-    let that = this
-    console.log(e)
-    let index = e.currentTarget.dataset.index
-    console.log("index",index)
-    if (index == 0) {
-      that.setData({
-        selected: 0
-      })
-    } else if (index == 1) {
-      that.setData({
-        selected: 1
-      })
-    } else {
-      that.setData({
-        selected: 2
-      })
-    } 
+
   },
 
   /**
